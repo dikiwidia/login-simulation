e5100d0c911b601f2042f0290dd0a64550247e18
Moch Diki Widianto <8361706+dikiwidia@users.noreply.github.com>
2026-05-14 08:54:38 +0700

    first commit

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
+require_once("koneksi.php");
 session_start();
 
 // keadaan login mengharuskan kita punya session aktif
 // $_SESSION['status_login'] = false;
+
+$page_name = $_GET['page_name'] ?? "dashboard";
 
 if (isset($_SESSION['status_login']) && $_SESSION['status_login']) {
     // jika login ada atau benar
@@ -24,11 +27,32 @@ if (isset($_SESSION['status_login']) && $_SESSION['status_login']) {
 </head>
 
 <body>
-    <div class="flex flex-col gap-4 items-center justify-center min-h-screen w-full bg-teal-600 text-white">
-        <h1 class="text-4xl font-bold">Selamat Datang di Dashboard</h1>
-        <a href="proses_logout.php"
-            class="inline-block px-2 py-1.5 text-white bg-rose-400 cursor-pointer rounded shadow">Keluar
-            Sistem</a>
+    <div class="flex flex-col gap-4 items-center min-h-screen w-full bg-teal-600 text-white p-5">
+        <div class="flex w-full bg-white rounded-lg min-h-40 overflow-hidden">
+            <div class="flex flex-col p-5 bg-teal-800 text-white w-1/5">
+                <ul class="flex flex-col w-full font-bold gap-4">
+                    <li><a href="index.php"
+                            class="hover:text-amber-200 <?php echo $page_name == 'dashboard' ? 'text-amber-300' : ''; ?>">Dashboard</a>
+                    </li>
+                    <li><a href="index.php?page_name=create_todo"
+                            class="hover:text-amber-200 <?php echo $page_name == 'create_todo' ? 'text-amber-300' : ''; ?>">Create
+                            New
+                            Todo</a>
+                    </li>
+                    <li><a href="proses_logout.php" class="hover:text-amber-200">Logout</a></li>
+                </ul>
+            </div>
+            <div class="flex flex-col p-5 flex-1 text-slate-800">
+                <?php
+                if ($page_name == "dashboard") {
+                    include("pages/page_dashboard.php");
+                }
+                if ($page_name == "create_todo") {
+                    include("pages/page_create_todo.php");
+                }
+                ?>
+            </div>
+        </div>
     </div>
 </body>
 
--- login.php
+++ login.php
@@ -31,7 +31,7 @@ if (isset($_SESSION['status_login']) && $_SESSION['status_login']) {
                 </div>
                 <div class="mb-4">
                     <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Password</label>
-                    <input type="text" id="password" name="password"
+                    <input type="password" id="password" name="password"
                         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                         placeholder="Password" required>
                 </div>
